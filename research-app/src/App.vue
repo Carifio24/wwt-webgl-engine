1189c16074495a2670e25b2fae2fd5cee5ecc384
Carifio24 <carifio24@gmail.com>
2022-08-25 14:24:05 -0400

    Change closestInView to calculate pixel distance of nearest point. Set app threshold in terms of pixels as well.

--- research-app/src/App.vue
+++ research-app/src/App.vue
@@ -347,7 +347,6 @@ import * as moment from "moment";
 import * as screenfull from "screenfull";
 import "vue-select/dist/vue-select.css";
 import { Buffer } from "buffer";
-import { debounce } from "debounce";
 import { Component, Prop, Watch } from "vue-property-decorator";
 import { mapGetters, mapMutations, mapState } from "vuex";
 
@@ -1165,7 +1164,7 @@ export default class App extends WWTAwareComponent {
   wwtComponentNamespace = wwtEngineNamespace;
   lastClosePt: RawSourceInfo | null = null;
   lastSelectedSource: Source | null = null;
-  distanceThreshold = 0.01;
+  distanceThreshold = 4;
   hideAllChrome = false;
   hipsUrl = "http://www.worldwidetelescope.org/wwtweb/catalog.aspx?W=hips"; // Temporary
   isMouseMoving = false;
@@ -1984,14 +1983,9 @@ export default class App extends WWTAwareComponent {
     return false;
   }
 
-  wwtOnMouseMove(event: MouseEvent) {
-    if (this.spreadsheetLayers.length == 0) {
-      return;
-    }
+  updateLastClosePoint(event: MouseEvent) {
     const pt = { x: event.offsetX, y: event.offsetY };
-    const raDecDeg = this.findRADecForScreenPoint(pt);
-    const raDecRad = { ra: D2R * raDecDeg.ra, dec: D2R * raDecDeg.dec };
-    const closestPt = this.closestInView(raDecRad, this.distanceThreshold);
+    const closestPt = this.closestInView(pt, this.distanceThreshold);
     if (closestPt == null && this.lastClosePt == null) {
       return;
     }
@@ -2003,6 +1997,13 @@ export default class App extends WWTAwareComponent {
     if (needsUpdate) {
       this.lastClosePt = closestPt;
     }
+  }
+
+  wwtOnMouseMove(event: MouseEvent) {
+    if (this.spreadsheetLayers.length == 0) {
+      return;
+    }
+    this.updateLastClosePoint(event);
     this.isMouseMoving = true;
   }
 
@@ -2011,12 +2012,15 @@ export default class App extends WWTAwareComponent {
   }
 
   wwtOnMouseUp(_event: MouseEvent) {
-    if (!this.isMouseMoving && this.lastClosePt !== null) {
+    if (this.isMouseMoving) {
+      this.isMouseMoving = false;
+      return;
+    }
+    if (this.lastClosePt !== null) {
       const source = this.sourceCreator(this.lastClosePt);
       this.addSource(source);
       this.lastSelectedSource = source;
     }
-    this.isMouseMoving = false;
   }
 
   newSourceName = (function () {
@@ -2872,7 +2876,7 @@ export default class App extends WWTAwareComponent {
   }
 
   closestInView(
-    target: AngleCoordinates,
+    point: { x: number; y: number },
     threshold?: number
   ): RawSourceInfo | null {
     let minDist = Infinity;
@@ -2880,6 +2884,9 @@ export default class App extends WWTAwareComponent {
 
     const rowSeparator = "\r\n";
     const colSeparator = "\t";
+
+    const raDecDeg = this.findRADecForScreenPoint(point);
+    const target = { ra: D2R * raDecDeg.ra, dec: D2R * raDecDeg.dec };
 
     for (const layerInfo of this.selectableTableLayers()) {
 
@@ -2916,9 +2923,16 @@ export default class App extends WWTAwareComponent {
         }
       }
     }
-    if (!threshold || minDist < threshold) {
-      return closestPt;
+
+    if (closestPt !== null) {
+      const closestRADecDeg = { ra: closestPt.ra * R2D, dec: closestPt.dec * R2D };
+      const closestScreenPoint = this.findScreenPointForRADec(closestRADecDeg);
+      const pixelDist = Math.sqrt((point.x - closestScreenPoint.x) ** 2 + (point.y - closestScreenPoint.y) ** 2);
+      if (!threshold || pixelDist < threshold) {
+        return closestPt;
+      }
     }
+
     return null;
   }
 
@@ -2926,20 +2940,6 @@ export default class App extends WWTAwareComponent {
     return {
       showPopover: false,
     };
-  }
-
-  /** The factor of 0.00002 converts between the WWT
-   * engine zoom and the distance between points.
-   * This value doesn't come from anywhere in particular,
-   * other than the cursor -> pointer change feeling natural
-   */
-  updateDistanceThreshold = debounce((app: App, newZoom: number) => {
-    app.distanceThreshold = 0.00002 * newZoom;
-  }, 20);
-
-  @Watch("wwtZoomDeg", { immediate: true })
-  onZoomChange(val: number) {
-    this.updateDistanceThreshold(this, val);
   }
 }
 </script>
