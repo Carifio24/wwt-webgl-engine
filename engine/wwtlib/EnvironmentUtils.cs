a9b4a42f3aed52759000817747b0b3c4d054d01d
Carifio24 <carifio24@gmail.com>
2023-02-21 10:07:21 -0500

    Add functions to detect whether window/document/navigator are defined.

---
+++ engine/wwtlib/EnvironmentUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class EnvironmentUtils
+{
+
+    public static bool isWindowUndefined()
+    {
+        return Script.IsUndefined(Script.GetScriptType(Script.Literal("window")));
+    }
+
+    public static bool isDocumentUndefined()
+    {
+        return Script.IsUndefined(Script.GetScriptType(Script.Literal("document")));
+    }
+
+    public static bool isNavigatorUndefined()
+    {
+        return Script.IsUndefined(Script.GetScriptType(Script.Literal("navigator")));
+    }
+}
